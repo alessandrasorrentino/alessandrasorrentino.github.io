418982c2d0b2f24f064ffd7f0847d2f9a7f9600d
alessandrasorrentino <18346149+alessandrasorrentino@users.noreply.github.com>
2021-07-09 09:54:40 +0100

    Update draw_eyes.js
    Random eyes configuration

--- js/draw_eyes.js
+++ js/draw_eyes.js
@@ -1,5 +1,18 @@
-var eyes_configurations=['0','1','2','3','4'];
-const eyes_conf='2';
+/*CONNECTION WITH THE SERVER
+var ws = new WebSocket("ws://127.0.0.1:5678/"),
+  messages = document.createElement('ul');
+  ws.onmessage = function (event) {
+    var messages = document.getElementsByTagName('ul')[0],
+    message = document.createElement('li'),
+    content = document.createTextNode(event.data);
+    message.appendChild(content);
+    messages.appendChild(message);
+    };
+document.body.appendChild(messages);
+*/
+
+var eyes_configurations=[0,1,2,3,4];
+const eyes_conf= eyes_configurations[Math.floor(Math.random()*eyes_configurations.length)]
 const radius=10; //original 20
 
 var canvas_eye_lu=document.getElementById("eye_l_u");
@@ -16,7 +29,7 @@ function draw_eye_l_u() {
     //var canvas = document.getElementById("eye_l_u");
     //console.log("I am here")
     var ctx = canvas_eye_lu.getContext("2d");
-    if (eyes_conf=='0' || eyes_conf=='2'){
+    if (eyes_conf==0 || eyes_conf==2){
       ctx.beginPath();
       ctx.arc(90, 120, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
@@ -38,7 +51,7 @@ function draw_eye_l_u() {
       ctx.fillStyle="blue"
       ctx.fill();
 	}
-	else if (eyes_conf=='3'){
+	else if (eyes_conf==3){
       ctx.beginPath();
       ctx.arc(180, 50, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
@@ -51,7 +64,7 @@ function draw_eye_l_u() {
       ctx.arc(270, 120, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
       ctx.fill();
-	}else if(eyes_conf=='4'){
+	}else if(eyes_conf==4){
       ctx.beginPath();
       ctx.arc(270, 120, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
@@ -62,7 +75,7 @@ function draw_eye_l_u() {
 }
 function draw_eye_r_u() {
     var ctx = canvas_eye_ru.getContext("2d");
-    if(eyes_conf=='0' || eyes_conf=='2'){
+    if(eyes_conf==0 || eyes_conf==2){
 	ctx.beginPath();
       ctx.arc(90, 120, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
@@ -83,7 +96,7 @@ function draw_eye_r_u() {
       ctx.arc(270, 120, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
       ctx.fill();
-        }else if (eyes_conf== '3'){
+        }else if (eyes_conf== 3){
 	ctx.beginPath();
       ctx.arc(90, 120, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
@@ -96,7 +109,7 @@ function draw_eye_r_u() {
       ctx.arc(180, 50, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
       ctx.fill();
-       }else if(eyes_conf=='4'){
+       }else if(eyes_conf==4){
 	ctx.beginPath();
       ctx.arc(90, 120, radius, 0, 2 * Math.PI);
       ctx.fillStyle="blue"
@@ -107,7 +120,7 @@ function draw_eye_r_u() {
 }
 function draw_eye_l_d() {
     var crd_ctx = canvas_eye_ld.getContext("2d");
-      if(eyes_conf=='1' || eyes_conf=='2'){
+      if(eyes_conf==1 || eyes_conf==2){
 	crd_ctx.beginPath();
         crd_ctx.arc(100, 20, radius, 0, 2 * Math.PI);
         crd_ctx.fillStyle="blue"
@@ -125,7 +138,7 @@ function draw_eye_l_d() {
         crd_ctx.fillStyle="blue"
         crd_ctx.fill();
       }
-       else if (eyes_conf=='4'){
+       else if (eyes_conf==4){
 	        crd_ctx.beginPath();
         crd_ctx.arc(210, 50, radius, 0, 2 * Math.PI);
         crd_ctx.fillStyle="blue"
@@ -140,7 +153,7 @@ function draw_eye_l_d() {
  } 
   function draw_eye_r_d() {
     var crd_ctx = canvas_eye_rd.getContext("2d");
-      if(eyes_conf=='1' || eyes_conf=='2'){
+      if(eyes_conf==1 || eyes_conf==2){
         crd_ctx.beginPath();
         crd_ctx.arc(100, 20, radius, 0, 2 * Math.PI);
         crd_ctx.fillStyle="blue"
@@ -157,7 +170,7 @@ function draw_eye_l_d() {
         crd_ctx.fillStyle="blue"
         crd_ctx.fill();
 
-     }else if (eyes_conf=='4'){
+     }else if (eyes_conf==4){
         crd_ctx.beginPath();
         crd_ctx.arc(100, 20, radius, 0, 2 * Math.PI);
         crd_ctx.fillStyle="blue"
